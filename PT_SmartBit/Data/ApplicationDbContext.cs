766b8dbe8dfaaecf0588b3e71a070e77d8f963b6
Duvan9908 <duvanrodriguez@usantotomas.edu.co>
2025-05-29 10:23:40 -0500

    Creación de Proyecto

---
+++ PT_SmartBit/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PT_SmartBit.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
